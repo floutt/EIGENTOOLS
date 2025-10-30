ef7bb4f79b1c0f49213f73ab362ece88f2183978
Oluwatosin Olayinka <demonslayerguy@gmail.com>
2025-10-30 14:31:28 -0400

    added documentation

--- src/EIGENTOOLS/_write.py
+++ src/EIGENTOOLS/_write.py
@@ -8,19 +8,43 @@ val_map = {0: 0, 1: 1, 2: 2, nan: 3}
 
 
 class PackedAncestryMapWriter:
+    """
+    Object designed for easy writing of a PackedAncestryMap file. This is done on a SNP record basis.
+
+    Attributes:
+        _nind (int): Number of individuals based on provided Ind_Info object
+        _nsnp (int): Number of variants based on provided SNP_Info object
+        _trailingbytes (int): Number of null bytes to write after record
+        _isclosed (bool): Boolean indicating if file is closed
+        _recordsleft (int): Number of records left to write
+    """
     def __init__(self, snp_obj: SNP_Info, ind_obj: Ind_Info,
                  geno_file: (str | None) = None, ind_file: (str | None) = None,
                  snp_file: (str | None) = None,
                  file_prefix: (str | None) = None, write_snp: bool = True,
                  write_ind: bool = True, write_header: bool = True):
+        """
+        Initialization method for PackedAncestryMapWriter object.
+
+        Args:
+            snp_obj (SNP_Info): Object containing variant information
+            ind_obj (Ind_Info): Object containing individual information
+            geno_file (str, optional): Output .geno file, cannot be used alongside "file_prefix" parameter
+            ind_file (str, optional): Output .ind file, cannot be used alongside "file_prefix" parameter
+            snp_file (str, optional): Output .snp file, cannot be used alongside "file_prefix" parameter
+            file_prefix (str, optional): Prefix for all PackedAncestryMap files. Will produce a ".ind", ".snp", and ".geno" file. Cannot be used alongside "geno_file", "ind_file", or "snp_file" parameters
+            write_snp (boolean): writes variant file if True
+            write_ind (boolean): writes individual file if True
+            write_header (boolean): writes PackedAncestryMap header if True
+        """
         # generate useful metadata
         self._nind = len(ind_obj)
         self._nsnp = len(snp_obj)
         header = ("GENO   %i %i %x %x" % (self._nind, self._nsnp,
                                           ind_obj._hash, snp_obj._hash)).encode()
-        self._min_byte_per_record = ceil(self._nind / 4)
-        self._recordsize = max(self._min_byte_per_record, len(header))
-        self._trailingbytes = bytes(self._recordsize - self._min_byte_per_record)
+        min_byte_per_record = ceil(self._nind / 4)
+        recordsize = max(min_byte_per_record, len(header))
+        self._trailingbytes = bytes(recordsize - min_byte_per_record)
         self._isclosed = False
         self._recordsleft = self._nsnp
 
@@ -49,9 +73,15 @@ class PackedAncestryMapWriter:
         # write header
         if write_header:
             self._fgeno.write(header)
-            self._fgeno.write(bytes(self._recordsize - len(header)))
+            self._fgeno.write(bytes(recordsize - len(header)))
 
     def write_record(self, dosage_list: list[Literal[0, 1, 2, nan]]):
+        """
+        Writes SNP record to PackedAncestryMap
+
+        Args:
+            dosage_list (list): List of allelic dosages. Length of list must be equal to the number of individuals.
+        """
         shift_by = 6
         record = 0
         # check length
@@ -75,10 +105,13 @@ class PackedAncestryMapWriter:
         if shift_by != 6:
             self._fgeno.write(bytes([record]))
         # write trailing bytes
-        self._fgeno.write(bytes(self._recordsize - self._min_byte_per_record))
+        self._fgeno.write(bytes(self._trailingbytes))
         self._recordsleft -= 1
 
     def close(self):
+        """
+        Closes PackedAncestryMap file object and marks object not suitable for writing
+        """
         if self._isclosed:
             return None
         else:
