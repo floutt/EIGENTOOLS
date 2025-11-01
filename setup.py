7897cc9cc3223eb71c85535519636a44a46ad42a
Oluwatosin Olayinka <demonslayerguy@gmail.com>
2025-11-01 11:16:11 -0400

    setup.py
    added setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="EIGENTOOLS",
+    version="0.1.0",
+    description="Python package designed for reading and writing PACKEDANCESTRY files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/floutt/EIGENTOOLS",
+    author="Oluwatosin Olayinka",
+    classifiers=[  # Optional
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Geneticists",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="EIGENSTRAT, PACKEDANCESTRYMAP, genetics, bioinformatics",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.11",
+    project_urls={
+        "Bug Reports": "https://github.com/floutt/EIGENTOOLS/issues",
+        "Source": "https://github.com/floutt/EIGENTOOLS",
+    },
+)
